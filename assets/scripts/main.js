9801f04ebc7ec28f62df1f94fcb69c9a6b2d31d8
mbjenson <matthew.jenson@protonmail.com>
2024-05-16 14:13:40 -0500

    removed unused assets and commented code

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,38 +1,9 @@
 
-// reveals element with id 'shown' and hides elements with ids in 'hidden'
 function reveal(shown, hidden) {
     hidden = hidden.length ? hidden : [hidden];
     for (var i = 0; i < hidden.length; i++) {
         document.getElementById(hidden[i]).style.display = 'none';
     }
     document.getElementById(shown).style.display = 'block';
-    // window.location.hash = shown; // this line causes the screen to jump a bit each time
     window.scrollTo(0, 0);
 }
-
-
-
-//var current;
-//= document.getElementById('projects-section');
-
-
-// function initialize(){
-//     current = document.getElementById("projects-section");
-// }
-
-// function reveal_projects() {
-//     // current.style.display = 'none';
-//     current = document.getElementById('projects-section');
-//     current.style.display = 'block';
-// }
-
-// function reveal_skills() {
-//     // current.style.display = 'none';
-//     current = document.getElementById('skills-section');
-//     current.style.display = 'block';
-// }
-
-// function test() {
-//     document.getElementById('demo').innerHTML = "<h1>hello world</h1>";
-// }
-
